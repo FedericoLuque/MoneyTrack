a2fa552aa1d40a6c8defa2ffca7caaac782135f1
Federico <fluque87@gmail.com>
2026-04-19 20:38:47 +0200

    feat: configure HiltWorkerFactory and disable default WorkManager init

--- app/src/main/java/com/federico/moneytrack/MoneyTrackApp.kt
+++ app/src/main/java/com/federico/moneytrack/MoneyTrackApp.kt
@@ -1,14 +1,25 @@
 package com.federico.moneytrack
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.federico.moneytrack.util.ThemeManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MoneyTrackApp : Application() {
+class MoneyTrackApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         ThemeManager.applyTheme(ThemeManager.getThemePreference(this))
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
